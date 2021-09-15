c127a1967fadc5e3feb4f20715d90942a42ac6c3
Rasmus Thorsøe <rt071195@gmail.com>
2021-09-15 11:35:10 +0200

    grahams scan, scan input

--- src/GrahamScan.java
+++ src/GrahamScan.java
@@ -6,6 +6,8 @@ import java.util.function.Function;
 public class GrahamScan {
 
     public List<Point> convex(List<Point> in) {
+        in.sort((p1, p2) -> Float.compare(p1.x, p2.x));
+        System.out.println(in);
         List<Point> out = new ArrayList<>();
         return out;
     }
