dd5e692adc98683699064b5378bd012e182514e4
locpe <locpeter@gmail.com>
2021-09-23 15:36:30 +0200

    First half of MBC algorithm

--- src/Marriage.java
+++ src/Marriage.java
@@ -5,6 +5,26 @@ public class Marriage implements CHAlgo {
 
     public MarriageResult convex(List<Point> in) {
         List<Point> out = new ArrayList<>();
+        if (in.size() < 2) return new MarriageResult(out);
+        float splitX = in.get(0).x - in.get(1).x;
+        List<Point> leftPoints = new ArrayList<>();
+        List<Point> rightPoints = new ArrayList<>();
+        for (Point point : in) {
+            if (point.x <= splitX) leftPoints.add(point);
+            else rightPoints.add(point);
+        }
+        List<Constraint2D> constraints = new ArrayList<>();
+        for (Point point : in) {
+            Constraint2D constraint = new Constraint2D();
+            constraint.a = new Point(-point.x, -1);
+            constraint.b = -point.y;
+            constraints.add(constraint);
+        }
+        // TODO: Indbyg at linjen skal krydse splitX mellem dens to punkter
+        LPSolver.LPResult res = new LPSolver().solve2D(in.get(0), splitX, 1, constraints);
+        if (!(res instanceof LPSolver.Good)) return new MarriageResult(out);
+        float lineA = ((LPSolver.Good) res).results[0];
+        float lineB = ((LPSolver.Good) res).results[1];
         return new MarriageResult(out);
     }
 
