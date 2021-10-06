029ee9cc75cf6bbb453ff6d25df9bf2cdec8e15a
locpe <locpeter@gmail.com>
2021-10-06 12:37:02 +0200

    LP2D virker

--- test/LPTest.java
+++ test/LPTest.java
@@ -82,4 +82,56 @@ class LPTest {
         assert result instanceof LPSolver.Infeasible;
     }
 
+    @Test
+    void testSinglePositive2d() {
+        LPSolver solver = new LPSolver();
+        Point initPoint = new Point(6, -4);
+        float c1 = 1.0f;
+        float c2 = 2.3f;
+        List<Constraint2D> constraints = new ArrayList<>();
+        Constraint2D constraint = new Constraint2D();
+        constraint.a = new Point(1.2f, -1.5f);
+        constraint.b = 12f;
+        constraints.add(constraint);
+        Constraint2D constraint2 = new Constraint2D();
+        constraint2.a = new Point(-1.2f, -1.5f);
+        constraint2.b = -3f;
+        constraints.add(constraint2);
+        LPSolver.LPResult result = solver.solve2D(initPoint, c1, c2, constraints);
+        System.out.println(result.getClass());
+        LPSolver.Good good = (LPSolver.Good) result;
+        for (float v : good.results) {
+            System.out.print(v + ", ");
+        }
+        System.out.println();
+    }
+
+    @Test
+    void testSinglePositive2d2() {
+        LPSolver solver = new LPSolver();
+        Point initPoint = new Point(6, -4);
+        float c1 = 0.4f;
+        float c2 = 0.5f;
+        List<Constraint2D> constraints = new ArrayList<>();
+        Constraint2D constraint = new Constraint2D();
+        constraint.a = new Point(0.3f, 0.1f);
+        constraint.b = 2.7f;
+        constraints.add(constraint);
+        Constraint2D constraint2 = new Constraint2D();
+        constraint2.a = new Point(-0.6f, -0.4f);
+        constraint2.b = -6f;
+        constraints.add(constraint2);
+        Constraint2D constraint3 = new Constraint2D();
+        constraint3.a = new Point(0.5f, 0.5f);
+        constraint3.b = 6f;
+        constraints.add(constraint3);
+        LPSolver.LPResult result = solver.solve2D(initPoint, c1, c2, constraints);
+        System.out.println(result.getClass());
+        LPSolver.Good good = (LPSolver.Good) result;
+        for (float v : good.results) {
+            System.out.print(v + ", ");
+        }
+        System.out.println();
+    }
+
 }
